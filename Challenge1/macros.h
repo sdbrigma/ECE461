687ee749f85a3b631f206a20076b306fdcad0569
Steffon Brigman <steffon.brigman@gmail.com>
2016-03-06 14:19:46 -0500

    committing current progress on challenges

---
+++ Challenge1/macros.h
@@ -0,0 +1,17 @@
+/*
+ * macros.h
+ *
+ *  Created on: Feb 5, 2016
+ *      Author: Steffon Brigman
+ */
+
+#ifndef CHALLENGE1_MACROS_H_
+#define CHALLENGE1_MACROS_H_
+
+
+#define BUFFER_MEMORY_START 0x20000
+//extern uint8_t buffer[4096];
+
+//uint32_t buttonPushed = 0;
+
+#endif /* CHALLENGE1_MACROS_H_ */
